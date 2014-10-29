8d934b49a96cdedd450fba662797bc4d028377c2
jamelablack <jamela.black@gmail.com>
2014-10-29 12:13:46 -0400

    added hit/stay buttons

--- tl-blackjack-webapp/main.rb
+++ tl-blackjack-webapp/main.rb
@@ -13,6 +13,35 @@ set :sessions, true
 # 	end
 # end
 
+helpers do #any methods defined within the helpers block are avaiable for both main.rb aswell as the templates
+
+
+	def calculate_total(cards)  #we need a method that accepts the array of cards and calculates a total
+		arr = cards.map{|e| e[1] }	
+
+		total = 0
+		arr.each do |value| 
+			if value == "A"
+				total += 11
+			elsif value.to_i == 0 #if card value has no integer value (equals 0), add 10
+				total += 10 #in instances of Jack, Queen, or King
+			else
+				total += value.to_i #if card has an integer value
+			end
+	end
+	#correct for Aces
+	arr.select{|e| e == "A"}.count.times do
+			total -= 10 if total > 21	
+		end
+	total
+	end
+	#calculate_total(session[:dealers_cards]) => 20
+end
+
+before do #set of instance variable before each action
+	@show_hit_or_stay_buttons = true
+end
+
 get '/' do 
 	if session[:player_name]# able to reconsitute semi-persistent state through a semi-persistent cookies. 
 		#progress to the game
@@ -48,4 +77,23 @@ get '/game' do
 			session[:player_cards] << session[:deck].pop
 			session[:player_cards] << session[:deck].pop
 	erb :game
+end
+
+post '/game/player/hit' do 
+	session[:player_cards] << session[:deck].pop
+	#instance variables are the best to use in this case to communicate to the layout template 
+	#because they go away as soon as another request comes in.
+	#@error is a perfect case for thiserb :game
+	if calculate_total(session[:player_cards]) > 21
+			@error = "Sorry, it looks like you busted."
+			@show_hit_or_stay_buttons = false
+		end
+		erb :game
+	end
+
+post '/game/player/stay' do 
+	@success = "You have chosen to stay!"
+	@show_hit_or_stay_buttons = false
+	erb :game
+
 end
